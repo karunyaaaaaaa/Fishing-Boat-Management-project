d4370ab58efb12604e6b15b64b3c3c0095eba839
karunya <karunya.id21@gmail.com>
2024-08-05 22:17:39 +0530

    Files Upload

---
+++ src/fishing/boat/management/system/Conn.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fishing.boat.management.system;
+
+/**
+ *
+ * @author asus
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    ResultSet rs;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/fms","root","123456karunE#");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to close the connection, statement, and result set
+    public void close() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (s != null) {
+                s.close();
+            }
+            if (c != null) {
+                c.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
